86c55959a1b3e51efc56c76aabe41cc384ba9413
hayashidareo <reo0524h@gmail.com>
2025-04-08 14:31:23 +0900

    詳細画面の土台作成

---
+++ lib/feature/view/repository_detail_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+
+class RepositoryDetailPage extends ConsumerWidget {
+  const RepositoryDetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Placeholder();
+  }
+}
