1441ee0be5c093eb053ad3054c8961ba3ec8af48
hayashidareo <reo0524h@gmail.com>
2025-04-08 20:07:09 +0900

    mainにgo_routerの設定を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:search_repositories/feature/view/repository_list_page.dart';
+import 'package:search_repositories/config/routing/app_router.dart';
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+  Widget build(BuildContext context, WidgetRef ref) {
+    final GoRouter appRouter = ref.watch(appRouterProvider);
+    return MaterialApp.router(
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const RepositoryListPage(),
+      routerConfig: appRouter,
     );
   }
 }
