0a27c157cd94583d17051251338e9ada407711ec
hayashidareo <reo0524h@gmail.com>
2025-04-08 14:30:33 +0900

    必要なパッケージのインストール、一覧ページの土台作成

---
+++ lib/feature/view/repository_list_page.dart
@@ -0,0 +1,11 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter/material.dart';
+
+class RepositoryListPage extends HookConsumerWidget {
+  const RepositoryListPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Placeholder();
+  }
+}
