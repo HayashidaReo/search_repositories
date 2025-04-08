785345bd316a8c364fa68a7c9bb8412bad4506e2
hayashidareo <reo0524h@gmail.com>
2025-04-08 14:47:26 +0900

    providerをscopeで囲む

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:search_repositories/feature/view/repository_list_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
