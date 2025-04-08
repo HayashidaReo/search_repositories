de73551207528dbaa130401ee86f4565bc60ab07
hayashidareo <reo0524h@gmail.com>
2025-04-08 14:16:56 +0900

    使用するpaddingを定義

---
+++ lib/config/util/custom_padding.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CustomPadding {
+  static const EdgeInsets smallest = EdgeInsets.all(10);
+  static const EdgeInsets mini = EdgeInsets.all(12);
+  static const EdgeInsets small = EdgeInsets.all(14);
+  static const EdgeInsets normal = EdgeInsets.all(16); // 通常
+  static const EdgeInsets large = EdgeInsets.all(22);
+  static const EdgeInsets largest = EdgeInsets.all(26);
+  static const EdgeInsets xLarge = EdgeInsets.all(32);
+}
