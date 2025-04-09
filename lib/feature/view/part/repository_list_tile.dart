d83a01780168dbf6d611ec8e258a0eb1bc2533fc
hayashidareo <reo0524h@gmail.com>
2025-04-09 17:58:18 +0900

    一覧画面のタイルを修正

--- lib/feature/view/part/repository_list_tile.dart
+++ lib/feature/view/part/repository_list_tile.dart
@@ -7,42 +7,65 @@ class RepositoryListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        repo.name,
-        maxLines: 1,
-        overflow: TextOverflow.ellipsis,
-        style: TextStyle(
-          color: ColorStyle.black,
-          fontSize: CustomFontSize.medium,
-        ),
-      ),
-      subtitle: Text(
-        repo.description ?? '詳細がありません',
-        maxLines: 2,
-        overflow: TextOverflow.ellipsis,
-        style: TextStyle(
-          color: ColorStyle.darkGrey,
-          fontSize: CustomFontSize.small,
-        ),
-      ),
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Icon(Icons.star, color: ColorStyle.yellow),
-          WidthMargin.mini,
-          SizedBox(
-            width: 40,
-            child: Text(
-              formatStars(repo.stars),
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-        ],
-      ),
+    return InkWell(
       onTap: () {
         context.pushNamed(AppRoute.repositoryDetail.name, extra: repo);
       },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ListTile(
+            title: Text(
+              repo.name,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                color: ColorStyle.black,
+                fontSize: CustomFontSize.medium,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(
+              repo.description ?? '詳細がありません',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                color: ColorStyle.darkGrey,
+                fontSize: CustomFontSize.small,
+              ),
+            ),
+          ),
+
+          HeightMargin.mini,
+          // 情報アイコン
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _iconInfo(Icons.star, repo.stars),
+              _iconInfo(Icons.remove_red_eye, repo.watchers),
+              _iconInfo(Icons.call_split, repo.forks),
+              _iconInfo(Icons.bug_report, repo.issues),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _iconInfo(IconData icon, int value) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(icon, size: 16, color: ColorStyle.darkGrey),
+        WidthMargin.mini,
+        Text(
+          formatStars(value),
+          style: TextStyle(
+            fontSize: CustomFontSize.small,
+            color: ColorStyle.darkGrey,
+          ),
+        ),
+      ],
     );
   }
 }
