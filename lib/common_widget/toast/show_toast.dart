c3030d67bc5dfc83be10f55e33fc8b46dca0a9b8
hayashidareo <reo0524h@gmail.com>
2025-04-10 11:54:20 +0900

    toastの作成

---
+++ lib/common_widget/toast/show_toast.dart
@@ -0,0 +1,15 @@
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:search_repositories/config/util/color_style.dart';
+import 'package:search_repositories/config/util/custom_font_size.dart';
+
+void showToast(String msg) {
+  Fluttertoast.showToast(
+    msg: msg,
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.TOP,
+    timeInSecForIosWeb: 1,
+    backgroundColor: ColorStyle.darkBlack,
+    textColor: ColorStyle.white,
+    fontSize: CustomFontSize.normal,
+  );
+}
