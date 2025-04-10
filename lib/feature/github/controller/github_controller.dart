a8bace808313d417ba1d7ac02c87675a64cce025
hayashidareo <reo0524h@gmail.com>
2025-04-10 11:42:21 +0900

    不要なimportを削除

--- lib/feature/github/controller/github_controller.dart
+++ lib/feature/github/controller/github_controller.dart
@@ -1,6 +1,5 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:search_repositories/config/key/secure_storage_key.dart';
-import 'package:search_repositories/feature/auth/controller/auth_controller.dart';
 import 'package:search_repositories/feature/auth/controller/secure_storage_controller.dart';
 import 'package:search_repositories/feature/github/model/api_response.dart';
 import 'package:search_repositories/feature/github/repo/search_github_repo.dart';
