1202ebc80046b8954f8adafd181b6ce62277290b
hayashidareo <reo0524h@gmail.com>
2025-04-14 21:40:52 +0900

    サインアウト、アカウント削除時にトークンも消すように

--- lib/feature/auth/controller/auth_controller.dart
+++ lib/feature/auth/controller/auth_controller.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:search_repositories/config/key/secure_storage_key.dart';
+import 'package:search_repositories/feature/auth/controller/secure_storage_controller.dart';
 import 'package:search_repositories/feature/auth/repo/auth_repo.dart';
 
 part 'auth_controller.g.dart';
@@ -19,10 +21,16 @@ class AuthController extends _$AuthController {
   /// サインアウト
   Future<void> signOut() async {
     await ref.read(authRepoProvider.notifier).signOut();
+    await ref
+        .read(secureStorageControllerProvider.notifier)
+        .deleteValue(key: SecureStorageKey.githubAccessToken);
   }
 
   /// アカウントを削除
   Future<void> delete() async {
     await ref.read(authRepoProvider.notifier).delete();
+    await ref
+        .read(secureStorageControllerProvider.notifier)
+        .deleteValue(key: SecureStorageKey.githubAccessToken);
   }
 }
