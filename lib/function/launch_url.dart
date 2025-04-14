2ee3a276735572ebaa09e3a67428da5a0a18fe60
hayashidareo <reo0524h@gmail.com>
2025-04-14 20:21:23 +0900

    launchInExternalBrowserの関数説明を追加

--- lib/function/launch_url.dart
+++ lib/function/launch_url.dart
@@ -1,5 +1,6 @@
 import 'package:url_launcher/url_launcher.dart';
 
+/// 引数で受け取ったURLを外部ブラウザで開く
 Future<void> launchInExternalBrowser(String url) async {
   final uri = Uri.parse(url);
   if (await canLaunchUrl(uri)) {
