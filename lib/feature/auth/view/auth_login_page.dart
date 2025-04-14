47c81da599c147a7f6695d3054c8bd35d750b0ed
hayashidareo <reo0524h@gmail.com>
2025-04-14 11:27:49 +0900

    githubに接続する時にローディングを回すように

--- lib/feature/auth/view/auth_login_page.dart
+++ lib/feature/auth/view/auth_login_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:search_repositories/common_widget/dialog/loading_dialog.dart';
 import 'package:search_repositories/common_widget/loading_widget.dart';
 import 'package:search_repositories/common_widget/toast/show_toast.dart';
 import 'package:search_repositories/config/key/secure_storage_key.dart';
@@ -77,8 +78,9 @@ class AuthLoginPage extends ConsumerWidget {
                     shadowColor: Theme.of(context).colorScheme.tertiary,
                   ),
                   onPressed: () async {
-                    //TODO: ローディングダイアログの表示
+                    showLoadingDialog('接続中...');
                     await _signInWithGitHub(ref, localizations);
+                    hideLoadingDialog();
                   },
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
